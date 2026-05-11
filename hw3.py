e25e01eec49323ddad8084d9d6fcf117cba4061b
Ji Young in <139129405+yeongin-ji@users.noreply.github.com>
2026-05-11 22:01:46 +0900

    Refactor camera transform configuration
    Updated camera transformation method to use Transform object.

--- hw3.py
+++ hw3.py
@@ -1,41 +1,38 @@
 from gpiozero import Button, MotionSensor
 from picamera2 import Picamera2
+from libcamera import Transform  # 1. Added for correct transform type
 from signal import pause
 from time import sleep
 import os
 
-# 1. Component Configuration
-# Button is connected to GPIO 2, PIR sensor to GPIO 4
+# Component Configuration
 button = Button(2)
 pir = MotionSensor(4)
 
-# 2. Picamera2 Initialization & Setup
+# Picamera2 Initialization & Setup
 pic = Picamera2()
 config = pic.create_preview_configuration(main={"size": (1280, 720)})
 
-# Set rotation for v1.3 camera
-config["transform"] = "rotate180" 
+# 2. Fixed: Use Transform object instead of a string
+config["transform"] = Transform(rotate180=True) 
+
 pic.configure(config)
 
-# 3. Start Camera Preview
+# Start Camera Preview
 pic.start()
-print("Camera preview started.")
+print("Camera preview started. System is ready.")
 
 # Variable for image indexing
 i = 0
 
-# Stop the camera and exit the program when the button is pressed
 def stop_camera():
     print("\nStopping camera and exiting...")
     pic.stop()
     os._exit(0) 
 
-# Take a photo when motion is detected
 def take_photo():
     global i
     i = i + 1
-    
-    # Path settings - make sure 'iot-team5' matches your username
     save_path = f'/home/iot-team5/Desktop/image_{i}.jpg'
     
     print(f'Motion Detected! Capturing: image_{i}.jpg')
@@ -49,7 +46,6 @@ button.when_pressed = stop_camera
 pir.when_motion = take_photo
 
 try:
-    print("System is Ready. Press the button to exit.")
     pause()
 except KeyboardInterrupt:
     pic.stop()
